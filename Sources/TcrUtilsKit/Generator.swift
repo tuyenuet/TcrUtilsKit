dba26d71e0a893b10ffb87c483498157608afc74
TuyenNM <tuyennm.uet@gmail.com>
2024-01-23 10:57:21 +0700

    Initial Commit

---
+++ Sources/TcrUtilsKit/Generator.swift
@@ -0,0 +1,17 @@
+//
+//  Generator.swift
+//  TcrSPM
+//
+//  Created by Tuyen Nguyen on 23/01/2024.
+//
+
+import Foundation
+//import RxSwift
+
+public struct Generator {
+    
+    public static func testEmit() {
+        print("testEmit func...")
+//        Observable.of(1, 5, 7, 8).subscribe(onNext: { print($0) }).dispose()
+    }
+}
