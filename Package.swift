ca06274ef2dcb9c49cfb7821b995653d0adc81d1
TuyenNM <tuyennm.uet@gmail.com>
2024-01-23 14:30:31 +0700

    Add TcrLib.xcframework

--- Package.swift
+++ Package.swift
@@ -9,13 +9,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TcrUtilsKit",
-            targets: ["TcrUtilsKit"]),
+            targets: ["TcrUtilsKit", "TcrLib"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "TcrUtilsKit"),
+        .binaryTarget(name: "TcrLib", path: "TcrLib.xcframework"),
         .testTarget(
             name: "TcrUtilsKitTests",
             dependencies: ["TcrUtilsKit"]),
---
+++ TcrLib.xcframework/ios-arm64_x86_64-simulator/TcrLib.framework/Headers/TcrLib.h
@@ -0,0 +1,18 @@
+//
+//  TcrLib.h
+//  TcrLib
+//
+//  Created by Tuyen Nguyen on 23/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TcrLib.
+FOUNDATION_EXPORT double TcrLibVersionNumber;
+
+//! Project version string for TcrLib.
+FOUNDATION_EXPORT const unsigned char TcrLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TcrLib/PublicHeader.h>
+
+
