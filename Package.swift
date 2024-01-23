81f75f20ab4e0004e5b2060ee96f670043a89ad9
TuyenNM <tuyennm.uet@gmail.com>
2024-01-23 14:56:45 +0700

    Add SnapKit dependencies

--- Package.swift
+++ Package.swift
@@ -11,11 +11,14 @@ let package = Package(
             name: "TcrUtilsKit",
             targets: ["TcrUtilsKit", "TcrLib"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TcrUtilsKit"),
+            name: "TcrUtilsKit", dependencies: ["SnapKit"]),
         .binaryTarget(name: "TcrLib", path: "TcrLib.xcframework"),
         .testTarget(
             name: "TcrUtilsKitTests",
